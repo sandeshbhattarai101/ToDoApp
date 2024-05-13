6efa5829b587d3c0a4c9e839a338c016886f4008
sandeshbhattarai <83899409+sandeshbhattarai101@users.noreply.github.com>
2024-05-13 21:31:15 +0545

    Update index.js

--- SynthbitBackend/index.js
+++ SynthbitBackend/index.js
@@ -16,7 +16,7 @@ const app = express();
 // CORS configuration
 const corsOptions = {
   // origin: "http://localhost:5173",
-   origin: ["https://to-do-app-mu-liart.vercel.app"],
+   origin: "https://to-do-app-mu-liart.vercel.app",
    methods: ["POST","GET", "PUT", "DELETE", "OPTIONS"],
   credentials: true,
 };
